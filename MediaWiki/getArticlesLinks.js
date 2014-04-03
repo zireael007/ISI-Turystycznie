d119f33d42968cbf4b463aa226b569b61ae4add3
bigsergey <bolszow@gmail.com>
2014-04-03 13:22:39 +0200

    jeden plik dla linkow, szukanie w głąb

--- MediaWiki/getArticlesLinks.js
+++ MediaWiki/getArticlesLinks.js
@@ -8,44 +8,90 @@ var client = new bot({
 	"debug": false
 });
 
-var writeToFile = function(file, data) {
+
+var numberOfFiles = 0;
+var numberOfLinks = 0;
+
+fs.writeFile('links', '', function(){console.log('Zawartośc pliku links została usunieta')})
+
+var writeToFile = function(dataName, data, cb) {
 	//write links to file
-	fs.writeFile(file, data, function(err) {
+	fs.appendFile('links', data + '\n', function(err) {
     	if(err) {
 	        console.log(err);
     	} else {
-        	console.log(file + " done!!!");
+        	if (cb) {
+        		cb();
+        	}
+        	console.log(dataName + " gotowe!!!");
+        	console.log('Liczba artykułów ' + (numberOfFiles++) +'.');
+        	console.log('Zapisano ' + (numberOfLinks) +' linków.\n');
     	}
 	}); 
 
 };
 
-var getLinks = function (articleName, fileName) {
+var getArticleLinks = function (articleName, deepStep) {
 	client.getArticle(articleName, function(data){
+		console.log('sciagam artykul ' + articleName);
 	 	var links = data.match(reg);
 	 	links.push(articleName);
 	 	for (var link in links) {
+	 		if (deepStep != 0) {
+	 			getArticleLinks(links[link].slice(2).slice(0,-2).replace(/\s/g, "_"), deepStep - 1);
+	 		}
 	 		links[link] = 'http://pl.wikipedia.org/wiki/' + links[link].slice(2).slice(0,-2).replace(/\s/g, "_");
 	 	}
 	 	links = _.uniq(links);
-	 	writeToFile(fileName, links.sort().join('\n'));
+	 	writeToFile('akrtykuł:'+ articleName, links.sort().join('\n'), function () {
+	 		console.log(articleName + ' - ' + links.length);
+	 		numberOfLinks += links.length;
+	 	});
 	});
+};
+
+var getCategoryLinks = function (categoryName, deepStep) {
+	client.getPagesInCategory(categoryName, function(data) {
+		console.log('sciągam kategorie' + categoryName);
+		data = _.pluck(data, 'title');
+		if (deepStep != 0) {
+			_.each(data, function(val){
+				if (val.match(/kategoria/gi)) {
+					getCategoryLinks(val.replace(/\s/g, "_").slice(10), deepStep - 1);
+				}	
+			});
+		}
+		data = _.map(data, function (elem) {
+			return 'http://pl.wikipedia.org/wiki/' + elem.replace(/\s/g, "_");
+		});
+		if (data.length != 0) {
+				writeToFile('kategoria:' + categoryName, data.join('\n'), function() {
+				console.log(categoryName + ' - ' + data.length);
+				numberOfLinks += data.length;
+			});
+		}
+	});	
 };
 
 var reg = /\[{2,2}(\w|[ąśłńęóÓżźćĄĘŃŻŹĆŁŚ]|\s)+\]{2,2}/g
 
 
 //artykuly dla analizy
-var articles = [ 
-	{
-		article: 'Miasta w Polsce',
-		fileName: 'miasta_polski'
-	}, 
-	{
-		article: 'Lista uzdrowisk w Polsce',
-		fileName: 'uzdrowiska_polski'
+var articles = 
+	[{
+		article: 'Miasta_w_Polsce',
+		deepStep: 1,
+	},{
+		article: 'Organizacje_turystyczne_w_Polsce',
+		deepStep: 0
 	}];
 
 for (var elem in articles) {
-	getLinks(articles[elem].article, articles[elem].fileName);
+	getArticleLinks(articles[elem].article.replace(/\s/g, "_"), articles[elem].deepStep);
+}
+
+var categories = ['Turystyka w Polsce', 'Kina w Polsce'];
+
+for (var category in categories) {
+	getCategoryLinks(categories[category].replace(/\s/g, "_"), 3);
 }
